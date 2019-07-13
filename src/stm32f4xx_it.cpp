b54b306c1d4ec28e92e3709eedd199220b748f43
Marcin <slawicz@echostar.pl>
2019-07-13 12:17:03 +0200

    UART class completed; not tested

--- src/stm32f4xx_it.cpp
+++ src/stm32f4xx_it.cpp
@@ -9,11 +9,10 @@
 */
 
 /* Includes ------------------------------------------------------------------*/
+
+#include "uart.h"
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx.h"
-#ifdef USE_RTOS_SYSTICK
-#include <cmsis_os.h>
-#endif
 #include "stm32f4xx_it.h"
 
 /* Private typedef -----------------------------------------------------------*/
@@ -28,15 +27,95 @@
 /******************************************************************************/
 
 /**
+  * @brief This function handles Non maskable interrupt.
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief This function handles Memory management fault.
+  */
+void MemManage_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief This function handles Pre-fetch fault, memory access fault.
+  */
+void BusFault_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
+void UsageFault_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief This function handles Debug monitor.
+  */
+void DebugMon_Handler(void)
+{
+}
+
+/**
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
   * @brief  This function handles SysTick Handler, but only if no RTOS defines it.
   * @param  None
   * @retval None
   */
 void SysTick_Handler(void)
 {
-	HAL_IncTick();
-	HAL_SYSTICK_IRQHandler();
-#ifdef USE_RTOS_SYSTICK
-	osSystickHandler();
-#endif
+    HAL_IncTick();
+    HAL_SYSTICK_IRQHandler();
+}
+
+/******************************************************************************/
+/* STM32F4xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32f4xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles USART2 global interrupt.
+  */
+void USART2_IRQHandler(void)
+{
+    HAL_UART_IRQHandler(UART::pUSART2->getUartHandle());
 }
