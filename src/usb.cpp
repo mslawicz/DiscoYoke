469124ac47d2a9a6751774a35a8378f6db46bc76
Marcin <slawicz@echostar.pl>
2019-07-25 09:42:08 +0200

    USB files created

---
+++ src/usb.cpp
@@ -0,0 +1,24 @@
+/*
+ * usb.cpp
+ *
+ *  Created on: 25.07.2019
+ *      Author: Marcin
+ */
+
+#include "usb.h"
+
+namespace USB
+{
+
+Device::Device()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+Device::~Device()
+{
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace USB */
---
+++ src/usb.h
@@ -0,0 +1,23 @@
+/*
+ * usb.h
+ *
+ *  Created on: 25.07.2019
+ *      Author: Marcin
+ */
+
+#ifndef USB_H_
+#define USB_H_
+
+namespace USB
+{
+
+class Device
+{
+public:
+    Device();
+    ~Device();
+};
+
+} /* namespace USB */
+
+#endif /* USB_H_ */
